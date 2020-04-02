6e18ce087fc7f38a3a7c91200ad3832f09dda9fd
vvshuai <35093533+vvshuai@users.noreply.github.com>
2020-04-02 22:33:55 +0800

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -13,8 +13,22 @@ public class MainTest {
     @Test
     public void SolveTest1(){
         Main main = new Main();
-        int n = 0;
-        int[] arr = {1,2,3};
+        int n = 5;
+        int[] arr = {1,2,3,4,5};
+        main.solve(n,arr);
+    }
+    @Test
+    public void SolveTest2(){
+        Main main = new Main();
+        int n = -2;
+        int[] arr = {1,-4,3};
+        main.solve(n,arr);
+    }
+    @Test
+    public void SolveTest3(){
+        Main main = new Main();
+        int n = 6;
+        int[] arr = {-3,1,4,-2,6,5};
         main.solve(n,arr);
     }
 }
